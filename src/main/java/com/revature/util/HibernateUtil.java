75007d602b92e0c0b6cd441f7c5c4af37067dc79
ECorpron <ecorpron@gmail.com>
2021-02-23 13:20:11 -0800

    renamed ConnectFact to HibernateUtil to match conventions

--- src/main/java/com/revature/util/HibernateUtil.java
+++ src/main/java/com/revature/util/HibernateUtil.java
@@ -6,7 +6,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-public class ConnectionFact {
+public class HibernateUtil {
     private static SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory()
