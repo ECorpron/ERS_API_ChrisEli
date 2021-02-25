fe22a7b02ac6426dcfcd434af2a69968f2bef517
ECorpron <ecorpron@gmail.com>
2021-02-25 11:43:02 -0800

    reconfigured to use programatic configuration of hibernate

--- src/main/java/com/revature/util/HibernateUtil.java
+++ src/main/java/com/revature/util/HibernateUtil.java
@@ -1,40 +1,52 @@
 package com.revature.util;
 
+import com.revature.models.Reimbursement;
+import com.revature.models.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
 
 public class HibernateUtil {
-    private static SessionFactory sessionFactory = buildSessionFactory();
-
-    private static SessionFactory buildSessionFactory()
-    {
-        try
-        {
-            if (sessionFactory == null)
-            {
-                StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
-                        .configure("hibernate.cfg.xml").build();
-
-                Metadata metaData = new MetadataSources(standardRegistry)
-                        .getMetadataBuilder()
-                        .build();
-
-                sessionFactory = metaData.getSessionFactoryBuilder().build();
-            }
-            return sessionFactory;
-        } catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
+    private static SessionFactory sessionFactory;
     public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
 
-    public static void shutdown() {
-        getSessionFactory().close();
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "org.postgresql.Driver");
+                settings.put(Environment.URL, System.getenv("url"));
+                settings.put(Environment.USER, System.getenv("user"));
+                settings.put(Environment.PASS, System.getenv("password"));
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL82Dialect");
+
+                //settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                //settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(Reimbursement.class);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
