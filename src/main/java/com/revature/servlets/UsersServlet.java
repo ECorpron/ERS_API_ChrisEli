1e479d472f87919e56de2239bb69faa79f970e6b
chris <armoredmoose@gmail.com>
2021-02-26 09:24:08 -0600

    added errordto

--- src/main/java/com/revature/servlets/UsersServlet.java
+++ src/main/java/com/revature/servlets/UsersServlet.java
@@ -1,6 +1,9 @@
 package com.revature.servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.dtos.ErrorResponse;
+import com.revature.exceptions.FieldNotUniqueException;
+import com.revature.exceptions.InvalidCredentialsException;
 import com.revature.models.Role;
 import com.revature.models.User;
 import com.revature.services.UserService;
@@ -22,54 +25,53 @@ public class UsersServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         PrintWriter writer = resp.getWriter();
         ObjectMapper mapper = new ObjectMapper();
         HttpSession session = req.getSession(false);
         User rqst = (session == null) ? null : (User) req.getSession(false).getAttribute("this-user");
         resp.setContentType("application/json");
-
         try {
-
             if (rqst == null || rqst.getUserRole() != Role.ADMIN.ordinal()) {
+                ErrorResponse err = new ErrorResponse(401,"Not authorized to post.")
                 resp.setStatus(401);
+                writer.write(mapper.writeValueAsString(err));
                 return;
             }
-
             User newUser = mapper.readValue(req.getInputStream(),User.class);
             userService.register(newUser);
-
             resp.setStatus(200);
-
-
-        } catch (RuntimeException e){
-            e.printStackTrace();
+        } catch (FieldNotUniqueException fnu){
+            ErrorResponse err = new ErrorResponse(409,fnu.getMessage());
             resp.setStatus(409); //conflict
+            writer.write(mapper.writeValueAsString(err));
+        } catch(InvalidCredentialsException ice) {
+            resp.setStatus(409);
+            ErrorResponse err = new ErrorResponse(409,ice.getMessage());
+            writer.write(mapper.writeValueAsString(err));
+        }catch(Exception e) {
+            ErrorResponse err = new ErrorResponse(418,e.getMessage());
+            resp.setStatus(418);
+            writer.write(mapper.writeValueAsString(err));
         }
+
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         PrintWriter writer = resp.getWriter();
         ObjectMapper mapper = new ObjectMapper();
         HttpSession session = req.getSession(false);
         User rqst = (session == null) ? null : (User) req.getSession(false).getAttribute("this-user");
         resp.setContentType("application/json");
-
         try {
 
             if (rqst == null || rqst.getUserRole() != Role.ADMIN.ordinal()) {
                 resp.setStatus(401);
                 return;
             }
-
             User updateUser = mapper.readValue(req.getInputStream(),User.class);
             userService.update(updateUser);
-
             resp.setStatus(200);
-
-
         } catch (RuntimeException e){
             e.printStackTrace();
             resp.setStatus(409); //conflict
